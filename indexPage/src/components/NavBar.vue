71411a00a6a390c771e762217cea74203c702721
woodsding <woodsding@tencent.com>
2023-03-02 20:07:46 +0800

    feat: add bookmark draft

---
+++ indexPage/src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/bookmark">bookmark</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style lang="less">
+header {
+  line-height: 1.5;
+  background: #fff;
+}
+.wrapper {
+  opacity: 0.05;
+  background: #fff;
+  &:hover {
+    background: aliceblue;
+    opacity: 1;
+  }
+}
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ indexPage/src/views/BookmarkView.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+const form = reactive({
+  title: "",
+  url: "",
+  tags: [],
+  note: "",
+  syncBrowser: true,
+});
+const handleSubmit = (data: any) => {
+  console.log(data);
+};
+</script>
+<template>
+  <a-form
+    ref="formRef"
+    :model="form"
+    layout="vertical"
+    :style="{ width: '600px' }"
+    @submit="handleSubmit"
+  >
+    <a-form-item field="title" tooltip="default fill pageTitle" label="Title">
+      <a-input
+        v-model="form.title"
+        placeholder="please enter your username..."
+      />
+    </a-form-item>
+    <a-form-item field="url" label="Url">
+      <a-input v-model="form.url" />
+    </a-form-item>
+    <a-form-item field="tags" label="Tags">
+      <a-select
+        v-model="form.tags"
+        multiple
+        allow-create
+        placeholder="select tags or press Enter to create new tag"
+      >
+        <a-option value="section one">Section One</a-option>
+        <a-option value="section two">Section Two</a-option>
+        <a-option value="section three">Section Three</a-option>
+      </a-select>
+    </a-form-item>
+    <a-form-item field="note" label="Note">
+      <a-textarea
+        v-model="form.note"
+        placeholder="please enter your message"
+        :auto-size="{
+          minRows: 2,
+        }"
+      />
+      <template #extra>
+        <div>建议输入你的个人理解或者内容要点</div>
+      </template>
+    </a-form-item>
+    <a-form-item field="isRead">
+      <a-checkbox v-model="form.syncBrowser">
+        sync browser bookmark
+      </a-checkbox>
+    </a-form-item>
+    <a-form-item>
+      <a-space>
+        <a-button html-type="submit">Submit</a-button>
+        <a-button @click="$refs.formRef.resetFields()">Reset</a-button>
+      </a-space>
+    </a-form-item>
+  </a-form>
+</template>
+
+<style>
+main {
+  width: 400px;
+}
+</style>
